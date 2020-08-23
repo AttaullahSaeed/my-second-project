85ac25519c3eaa81da8fe16e65e6dc9e01dc82fa
Atta Ullah Saeed <attaullahsaeed77@gmail.com>
2020-08-23 17:22:46 +0500

    Updates

---
+++ precache-manifest.a4dd49088137fb66c645b6a8a09b5ecd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927ce1e68daa6ff0ecaef741725fb675",
+    "url": "/my-second-project/index.html"
+  },
+  {
+    "revision": "cdae60923aacb1a62b78",
+    "url": "/my-second-project/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "f90e744cf80812d69164",
+    "url": "/my-second-project/static/css/main.ed255db4.chunk.css"
+  },
+  {
+    "revision": "cdae60923aacb1a62b78",
+    "url": "/my-second-project/static/js/2.38f415d0.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/my-second-project/static/js/2.38f415d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f90e744cf80812d69164",
+    "url": "/my-second-project/static/js/main.9e35dd78.chunk.js"
+  },
+  {
+    "revision": "6395732d3a496cc99036",
+    "url": "/my-second-project/static/js/runtime-main.89cf0511.js"
+  },
+  {
+    "revision": "15ac2b3aaa74314814e5b75f87313bba",
+    "url": "/my-second-project/static/media/meo.15ac2b3a.jpg"
+  },
+  {
+    "revision": "04c086ddcbb6d56e4c6ce0c16e123eb0",
+    "url": "/my-second-project/static/media/meo1.04c086dd.jpg"
+  },
+  {
+    "revision": "bd9a221c5afce35b4770967b2c612763",
+    "url": "/my-second-project/static/media/meo3.bd9a221c.jpg"
+  },
+  {
+    "revision": "c7f310b011713c76d61749c4e7136bd4",
+    "url": "/my-second-project/static/media/meo4.c7f310b0.jpg"
+  },
+  {
+    "revision": "4b837e5e1fdffe0208b557eddcc06223",
+    "url": "/my-second-project/static/media/meo5.4b837e5e.jpg"
+  },
+  {
+    "revision": "e225b1469550e3a0f75885574d390b11",
+    "url": "/my-second-project/static/media/meo6.e225b146.jpg"
+  }
+]);
