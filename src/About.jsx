c4eef841debecb8217c090d155bdd041e9c40eb3
Atta Ullah Saeed <attaullahsaeed77@gmail.com>
2020-08-23 17:08:21 +0500

    my second react project

--- src/About.jsx
+++ src/About.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import la from '../src/images/la.jpg';
 
 import Common from './Common';
-import './ab.css'
+
 
 
 const About = () =>{
   return(
     <>
-     <Common name="Welcome to About Page" className="jan" imgsrc={la} visit="/contact" btnname="Contact Now"/>
+     <Common name="Welcome to About Page"  imgsrc={la} visit="/contact" btnname="Contact Now"/>
    
     </>
   )
